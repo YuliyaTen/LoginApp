39f8f497921f20bba7fe18eaec1bf47ad8dd4bad
2510-day <juliia.ten@gmail.com>
2022-07-05 19:03:49 +0300

    Setup 2

--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -2,11 +2,23 @@
 //  WelcomeViewController.swift
 //  LoginApp
 //
-//  Created by Yuliya Ten on 03.07.2022.
+//  Created by Yuliya Ten on 04.07.2022.
 //
 
 import UIKit
 
 class WelcomeViewController: UIViewController {
 
-  
+    var user: String?
+    @IBOutlet var userLable: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let user = self.user else { return  }
+        userLable.text = "Welcome, \(user)"
+    }
+    
+    @IBAction func logOutButtonTapped() {
+    }
+}
